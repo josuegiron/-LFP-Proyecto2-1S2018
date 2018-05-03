ed550f8081d16b1fbc3271fa391c514e0bceb9e2
MacBook Pro <benjamingiron1@gmail.com>
2018-05-03 16:06:15 -0600

    Gramatica terminada

--- GRAMATICA.go
+++ GRAMATICA.go
@@ -7,7 +7,7 @@ No Terminales = { }
 
 S = <INICIO>, 
 
-P = {
+Producciones = {
 	<INICIO> ::= '<' 'diseño' '>' <VARIABLES> <CONSTRUCCION> '<' '/' 'diseño' '>'
 
 	<COMENTARIO> ::= <COMENTARIO_LINEAL> | <COMENTARIO_MULTILINEA> | <EPSILON>
@@ -22,42 +22,34 @@ P = {
 	<EXPRESION> ::= numero operador numero <OTRA_EXPRESION>
 	<OTRA_EXPRESION> ::= operador numero <OTRA_EXPRESION> | <EPSILON>
 
-	<CONSTRUCCION> ::= '<' 'construccion' '>' <TERRENO> <PARED> <VENTANA> <PUERTA> <SUELO> '<' '/' 'construccion' '>'
+	<CONSTRUCCION> ::= '<' 'construccion' '>' <TERRENO> <PARED_S> <VENTANA_S> <PUERTA_S> <SUELO> '<' '/' 'construccion' '>'
+	
 	<TERRENO> ::= '<' 'terreno' '>' <ANCHO> <LARGO> '<' '/' 'terreno' '>'
 	<ANCHO> ::= 'ancho' '=' numero
 	<LARGO> ::= 'largo' '=' numero
-	<PARED> ::= '<' 'pared' '>' <CUERPO_PARED> '<' '/' 'pared' '>'
-	<CUERPO_PARED> ::= <PARED
 
+	<PARED_S> ::= '<' 'pared' '>' <CUERPO_PARED_S> '<' '/' 'pared' '>'
+	<CUERPO_PARED> ::= <PARED> <OTRA_PARED>
+	<OTRA_PARED> ::= ';' <PARED> <OTRA_PARED> | <EPSILON>
+	<PARED> ::= <NOMBRE> <COLOR> <ALTO> <INICIO> <FIN>
+	<COLOR> ::= 'color' ':' ('azul' | 'amarillo' | 'verde' | 'cafe')
+	<ALTO> ::= 'alto' ':' numero
+	<INICIO> ::= 'inicio' ':' numero ',' numero
+	<FIN> ::= 'fin' ':' numero ',' numero
+	 
+	<VENTANA_S> ::= '<' 'ventana' '>' <CUERPO_VENTANA> '<' '/' 'ventana' '>'
+	<CUERPO_VENTANA> ::= <VENTANA> <OTRA_VENTANA>
+	<OTRA_VENTANA> ::= ';' <VENTANA> <OTRA_VENTANA> | <EPSILON>
+	<VENTANA> ::= <NOMBRE> <TIPO> (<LONGITUD> | <RADIO>) <PARED_AOCIADA>
+	<LONGITUD> ::= 'longitud' ':' numero
+	<LONGITUD> ::= 'radio' '=' numero
+	<PARED_AOCIADA> ::= 'pared_asociada' ':' id
 
+	<PUERTA_S> ::= '<' 'puerta' '>' <CUERPO_PUERTA> '<' '/' 'puerta' '>'
+	<CUERPO_PUERTA> ::= <PUERTA> <OTRA_PUERTA>
+	<OTRA_PUERTA> ::= ';' <PUERTA> <OTRA_PUERTA> | <EPSILON>
+	<PUERTA> ::= <NOMBRE> <ALTO> <ANCHO> <PARED_AOCIADA> <COLOR> 
 
+	<SUELO> ::= '<' 'suelo' '>' <NOMBRE>  <COLOR>'<' '/' 'suelo' '>'
 
-
-
-	<MULTIPLICACION> ::= 'Multiplicar' <OPERACION_BINARIA>
-	<DIVISION> ::= 'Dividir' <OPERACION_BINARIA>
-	<POTENCIA> ::= 'Potencia' <OPERACION_BINARIA>
-	<RAIZCUADRADA> ::= 'RaizCuadrada' <OPERACION_UNARIA> 
-	<SENO> ::= 'Seno' <OPERACION_UNARIA> 
-	<COSENO> ::= 'Coseno' <OPERACION_UNARIA> 
-	<TANGENTE> ::= 'Tangente' <OPERACION_UNARIA>
-	<OPERACION_BINARIA> ::= '(' (<OPERACION> | numero | id) ',' (<OPERACION> | numero | id) ')'
-	<OPERACION_UNARIA> ::= '(' <OPERACION> | numero | id ')'
-	// FUNCIONES DE <FUNCIONES>
-	<FUNCION> ::= 'Inicio' 'Funcion' <COMENTARIO> <NOMBRE> ',' <COMENTARIO> <VALOR> <COMENTARIO> 'Fin' 'Funcion' <COMENTARIO>
-	<OTRA_FUNCION> ::= <FUNCION> <OTRA_FUNCION> | <EPSILON>
-	// FUNCIONES DE <GRAFICAS>
-	<GRAFICA> ::= 'Inicio' 'grafica' <COMENTARIO> <NOMBRE> ',' <X_POSITIVO> ',' <COMENTARIO> <X_NEGATIVO> ',' <COMENTARIO> <Y_POSITIVO> ',' <COMENTARIO> <Y_NEGATIVO> ',' <COMENTARIO> <ANCHO> ',' <COMENTARIO> <LARGO> ',' <COMENTARIO> <RUTA> ',' <COMENTARIO> <NOMBRE_FUNCION> <COMENTARIO> 'Fin' 'grafica' <COMENTARIO>
-	<X_POSITIVO> ::= 'x_positivo' = numero | <OPERACION> | id
-	<X_NEGATIVO> ::= 'x_negativo' = numero | <OPERACION> | id
-	<Y_POSITIVO> ::= 'y_positivo' = numero | <OPERACION> | id
-	<Y_NEGATIVO> ::= 'y_negativo' = numero | <OPERACION> | id
-	<ANCHO> ::= 'ancho' = numero | <OPERACION> | id
-	<LARGO> ::= 'largo' = numero | <OPERACION> | id
-	<RUTA> ::= 'ruta' = <CADENA>
-	<NOMBRE_FUNCION> ::= 'funcion' = id
-	<CADENA> ::= cadena
-	// Comentarios
-	<COMENTARIO_LINEAL> ::= comentarioLineal
-	<COMENTARIO_MULTILINEA> ::= comentarioMultilieal
-}
+	}
